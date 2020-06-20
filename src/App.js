b19e6594047b1c1583c29cffddca8bfaeac28d3d
GuiMM <guilherme.macedomoreira@gmail.com>
2020-06-20 16:48:46 -0300

    Implementa funções de listagem, criacao e remocao de repositorio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, {useState, useEffect} from 'react';
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+    useEffect(() => {
+        api.get('/repositories').then(response =>{
+          setRepositories(response.data);
+        })
+    }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories',{
+      title: `novo projeto ${Date.now()}`,
+      url: 'http://guiguis.com',
+      techs: 'teste'
+    });
+
+    const repo = response.data;
+    setRepositories([...repositories, repo]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`/repositories/${id}`).then();
+    
+    const newrepo = repositories.filter(x=> x.id!==id);
+    setRepositories([...newrepo]);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository=> 
+          <li key={repository.id}>{repository.title}
+            <button key={repository.id} onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>)
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
